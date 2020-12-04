4217b0c3f4a806ca6045d4bf32ba9b9ded70f531
clearloop <udtrokia@gmail.com>
2020-12-04 12:01:24 +0800

    feat(jupiter-io): abandon macro solution

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -3,38 +3,86 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use sp_runtime_interface::runtime_interface;
 
-macro_rules! zk {
-    ($($curve:ident),*) => {
-        /// ZK-SNARKs runtime interface
-        #[runtime_interface]
-        pub trait ZkSnarks {
-            $(
-                paste::item!{
-                    fn [<$curve _add>] () {
-                        megaclite::tests::$curve_add();
-                    }
+/// ZK-SNARKs runtime interface
+#[runtime_interface]
+pub trait ZkSnarks {
+    fn bls12_377_add() {
+        megaclite::tests::bls12_377_add();
+    }
 
-                    fn [<$curve _mul>] () {
-                        megaclite::tests::$curve_mul();
-                    }
+    fn bls12_377_mul() {
+        megaclite::tests::bls12_377_mul();
+    }
 
-                    fn [<$curve _pairing_two>]() {
-                        megaclite::tests::$curve_pairing();
-                    }
+    fn bls12_377_pairing_two() {
+        megaclite::tests::bls12_377_pairing();
+    }
 
-                    fn [<$curve _pairing_six()>] {
-                        megaclite::tests::$curve_pairing_six();
-                    }
-                }
-            )*
-        }
-    };
-}
+    fn bls12_377_pairing_six() {
+        megaclite::tests::bls12_377_pairing_six();
+    }
 
-zk! {
-    bls12_377,
-    bls12_381,
-    bn254,
-    bw6_761,
-    cp6_782
+    fn bls12_381_add() {
+        megaclite::tests::bls12_381_add();
+    }
+
+    fn bls12_381_mul() {
+        megaclite::tests::bls12_381_mul();
+    }
+
+    fn bls12_381_pairing_two() {
+        megaclite::tests::bls12_381_pairing();
+    }
+
+    fn bls12_381_pairing_six() {
+        megaclite::tests::bls12_381_pairing_six();
+    }
+
+    fn bn254_add() {
+        megaclite::tests::bn254_add();
+    }
+
+    fn bn254_mul() {
+        megaclite::tests::bn254_mul();
+    }
+
+    fn bn254_pairing_two() {
+        megaclite::tests::bn254_pairing();
+    }
+
+    fn bn254_pairing_six() {
+        megaclite::tests::bn254_pairing_six();
+    }
+
+    fn bw6_761_add() {
+        megaclite::tests::bw6_761_add();
+    }
+
+    fn bw6_761_mul() {
+        megaclite::tests::bw6_761_mul();
+    }
+
+    fn bw6_761_pairing_two() {
+        megaclite::tests::bw6_761_pairing();
+    }
+
+    fn bw6_761_pairing_six() {
+        megaclite::tests::bw6_761_pairing_six();
+    }
+
+    fn cp6_782_add() {
+        megaclite::tests::cp6_782_add();
+    }
+
+    fn cp6_782_mul() {
+        megaclite::tests::cp6_782_mul();
+    }
+
+    fn cp6_782_pairing_two() {
+        megaclite::tests::cp6_782_pairing();
+    }
+
+    fn cp6_782_pairing_six() {
+        megaclite::tests::cp6_782_pairing_six();
+    }
 }
